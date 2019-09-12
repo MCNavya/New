40ed7d4472b274cd0fd9e49831540fded184d406
MCNavya <navyamc0223@gmail.com>
2019-09-12 15:25:02 +0530

    initial commit

---
+++ Recruitment_Application/Recruitment_Application/Controllers/HiringManagerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recruitment_Application.Controllers
+{
+    public class HiringManagerController : Controller
+    {
+        // GET: HiringManager
+        public ActionResult HireManager()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult HireManager()
+        //{
+        //    return View();
+        //}
+    }
+}
